36a0d8b9ed21b0fe39b5c9ebe7d89bb71f853b72
helong.gao <helong.gao@sinobbd.com>
2017-07-18 19:38:34 +0800

    react: propTypes

--- src/main.jsx
+++ src/main.jsx
@@ -2153,34 +2153,186 @@ componentDidMount() {
 /**
  * @desc 基础实例.
  */
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+// 
+// class Greeting extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+// 
+//     render() {
+//         return (
+//             <h1>Hello, {this.props.name}</h1>
+//         );
+//     }
+// }
+// 
+// Greeting.propTypes = {
+//     name: PropTypes.string
+// };
+// 
+// // Type Wrong.
+// // const obj = {name: '123'}
+// ReactDOM.render(
+//     // Type Wrong.
+//     // <Greeting name={obj} />,
+// 
+//     // Correct.
+//     <Greeting name="Long Cloud" />,
+//     document.getElementById('app')
+// );
 
-class Greeting extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return (
-            <h1>Hello, {this.props.name}</h1>
-        );
-    }
-}
 
-Greeting.propTypes = {
-    name: PropTypes.string
-};
+/**
+ * @desc 官方示例.
+ */
+// import PropTypes from 'prop-types';
+// MyComponent.propTypes = {
+//     // You can declare that a prop is a specific JS primitive. By default, these
+//     // are all optional.
+//     optionalArray: PropTypes.array,
+//     optionalBool: PropTypes.bool,
+//     optionalFunc: PropTypes.func,
+//     optionalNumber: PropTypes.number,
+//     optionalObject: PropTypes.object,
+//     optionalString: PropTypes.string,
+//     optionalSymbol: PropTypes.symbol,
+// 
+//     // Anything that can be rendered: numbers, strings, elements or an array
+//     // (or fragment) containing these types.
+//     optionalNode: PropTypes.node,
+// 
+//     // A React element.
+//     optionalElement: PropTypes.element,
+// 
+//     // You can also declare that a prop is an instance of a class. This uses
+//     // JS's instanceof operator.
+//     optionalMessage: PropTypes.instanceOf(Message),
+// 
+//     // You can ensure that your prop is limited to specific values by treating
+//     // it as an enum.
+//     optionalEnum: PropTypes.oneOf(['News', 'Photos']),
+// 
+//     // An object that could be one of many types
+//     optionalUnion: PropTypes.oneOfType([
+//         PropTypes.string,
+//         PropTypes.number,
+//         PropTypes.instanceOf(Message)
+//     ]),
+// 
+//     // An array of a certain type
+//     optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+// 
+//     // An object with property values of a certain type
+//     optionalObjectOf: PropTypes.objectOf(PropTypes.number),
+// 
+//     // An object taking on a particular shape
+//     optionalObjectWithShape: PropTypes.shape({
+//         color: PropTypes.string,
+//         fontSize: PropTypes.number
+//     }),
+// 
+//     // You can chain any of the above with `isRequired` to make sure a warning
+//     // is shown if the prop isn't provided.
+//     requiredFunc: PropTypes.func.isRequired,
+// 
+//     // A value of any data type
+//     requiredAny: PropTypes.any.isRequired,
+// 
+//     // You can also specify a custom validator. It should return an Error
+//     // object if the validation fails. Don't `console.warn` or throw, as this
+//     // won't work inside `oneOfType`.
+//     customProp: function(props, propName, componentName) {
+//         if (!/matchme/.test(props[propName])) {
+//             return new Error(
+//                     'Invalid prop `' + propName + '` supplied to' +
+//                     ' `' + componentName + '`. Validation failed.'
+//                     );
+//         }
+//     },
+// 
+//     // You can also supply a custom validator to `arrayOf` and `objectOf`.
+//     // It should return an Error object if the validation fails. The validator
+//     // will be called for each key in the array or object. The first two
+//     // arguments of the validator are the array or object itself, and the
+//     // current item's key.
+//     customArrayProp: PropTypes.arrayOf(function(propValue, key, componentName, location, propFullName) {
+//         if (!/matchme/.test(propValue[key])) {
+//             return new Error(
+//                     'Invalid prop `' + propFullName + '` supplied to' +
+//                     ' `' + componentName + '`. Validation failed.'
+//                     );
+//         }
+//     })
+// };
+// 
+// ReactDOM.render(
+//     <Greeting name="Long Cloud" />,
+//     document.getElementById('app')
+// );
 
-// Type Wrong.
-// const obj = {name: '123'}
-ReactDOM.render(
-    // Type Wrong.
-    // <Greeting name={obj} />,
 
-    // Correct.
-    <Greeting name="Long Cloud" />,
-    document.getElementById('app')
-);
+
+/**
+ * @desc PropTypes.element.isRequired.
+ */
+// import PropTypes from 'prop-types';
+// 
+// class MyComponent extends React.Component {
+//     render() {
+//         // 这一定是一个元素, 如果不是会发出警告.
+//         const children = this.props.children;
+//         return (
+//            <div>
+//                 {children}
+//            </div>
+//         );
+//     }
+// }
+// 
+// MyComponent.propTypes = {
+//     children: PropTypes.element.isRequired
+// };
+// 
+// ReactDOM.render(
+//     <MyComponent>
+//         <div>PropTypes.element.isRequired</div>
+//     </MyComponent>,
+//     document.getElementById('app')
+// );
+
+
+
+/**
+ * @desc 默认值.
+ */
+// class Greeting extends React.Component {
+//     render() {
+//         return (
+//             <h1>Hello, {this.props.name}</h1>
+//         );
+//     }
+// }
+// 
+// // 给props设置默认值.
+// Greeting.defaultProps = {
+//     name: 'Stranger'
+// };
+// 
+// ReactDOM.render(
+//     <Greeting />,
+//     document.getElementById('app')
+// );
+
+
+
+
+
+
+
+
+
 
 
 
