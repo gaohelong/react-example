e53ea3144c55adabae7719684bbf7381970f698e
helong.gao <helong.gao@sinobbd.com>
2017-07-24 20:36:21 +0800

    react: react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
 /* react */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BasicExample from './router/BasicExample';
 
 /* sass */
 import './sass/main';
 import './sass/modules/main/main';
 
+/**
+ * @desc BasicExample.
+ */
+// import BasicExample from './router/BasicExample';
+// ReactDOM.render(
+//     <BasicExample />,
+//     document.getElementById('app')
+// );
+
+
+
+/**
+ * @desc ParamsExample.
+ */
+// import ParamsExample from './router/ParamsExample';
+// ReactDOM.render(
+//     <ParamsExample />,
+//     document.getElementById('app')
+// );
+
+/**
+ * @desc Redirects(Auth).
+ */
+import Redirects from './router/Redirects';
 ReactDOM.render(
-    <BasicExample />,
+    <Redirects />,
     document.getElementById('app')
 );
--- src/router/BasicExample.jsx
+++ src/router/BasicExample.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const BasicExample = () => (
     <Router>
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><Link className="link" to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/topics">Topics</Link></li>
             </ul>
@@ -29,38 +29,44 @@ const About = () => (
     </div>
 );
 
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>
-                    Rendering with React
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>
-                    Components
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>
-                    Props v. State
-                </Link>
-            </li>
-        </ul>
+const Topics = ({match}) => {
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li>
+                    <Link to={`${match.url}/rendering`}>
+                        Rendering with React
+                    </Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/components`}>
+                        Components
+                    </Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-v-state`}>
+                        Props v. State
+                    </Link>
+                </li>
+            </ul>
 
-        <Route path={`${match.url}/:topicId`} component={Topic}/>
-        <Route exact path={match.url} render={() => (
-            <h3>Please select a topic.</h3>
-        )}/>
-    </div>
-);
+            <Route path={`${match.url}/:topicId`} component={Topic}/>
+            <Route exact path={match.url} render={() => (
+                <h3>Please select a topic.</h3>
+            )}/>
+        </div>
+    );
+};
 
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-);
+const Topic = (obj) => {
+    let {match} = obj;
+    
+    return (
+        <div>
+            <h3>{match.params.topicId}</h3>
+        </div>
+    );
+};
 
 export default BasicExample;
