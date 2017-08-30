bf1f4d6503be65261504307cb1ff308262af3e2f
helong.gao <helong.gao@sinobbd.com>
2017-08-30 13:06:01 +0800

    react: fetch cors、json-server……

--- api/user.php
+++ api/user.php
@@ -1,10 +1,15 @@
 <?php
+// header("Access-Control-Allow-Origin: *"); // 支持fetch cors请求，不限制请求来源.
+header("Access-Control-Allow-Origin: http://localhost:8080"); // 支持fetch cors请求，并且限制请求来源为http://localhost:8080.
+
+// var_dump($_POST);
+// var_dump($_GET);
 $res = array(
-    "code" => 0,
-    "token" => "test2017",
-    "userinfo" => array(
-        "name" => "Ms. Madisyn Leannon",
-        "age" => 18
+    'code' => 0,
+    'token' => 'test2017',
+    'userinfo' => array(
+        'name' => 'MsLeannon',
+        'age' => 20
     )
 );
 
--- src/redux/Actions/Admin/login.js
+++ src/redux/Actions/Admin/login.js
@@ -36,52 +36,29 @@ export const loginFetch = (dispatch, data) => {
 
         // [fetch post]
         // post params: php $_POST.
-        // let formData = new FormData();
-        // formData.append('user', data.data.user);
-        // formData.append('pwd', data.data.pwd);
-        //
-        // fetch('http://hl.react.com/api/user.php', {
-        //     method: 'POST',
-        //     // body: JSON.stringify(data.data),
-        //     body: formData,
-        //     mode: 'no-cors', // 跨域.
-        //     headers: {
-        //         // 'Content-Type': 'application/x-www-form-urlencoded',
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //         // 'Content-Type': 'text/plain'
-        //     }
-        // }).then(function(response) {
-        //     console.log(response);
-        //     return response.json();
-        // }).then(function(json) {
-        //     console.log(json);
-        //     if (json.code === 0) { // 登录成功.
-        //         // dispatch({
-        //         //     type: LOGIN,
-        //         //     data: {
-        //         //         token: json.token,
-        //         //         userinfo: json.userinfo
-        //         //     }
-        //         // });
-        //     } else { // 登录失败.
-        //         // dispatch();
-        //     }
-        // }).catch(function(ex) {
-        //     console.log('parsing failed', ex);
-        //     // dispatch();
-        // });
+        let formData = new FormData();
+        formData.append('user', data.data.user);
+        formData.append('pwd', data.data.pwd);
 
-        // [fetch get]
-        // get params: php $_GET.
-        // fetch('http://localhost:8081/login?id=1&type=2', {
-        // fetch('http://hl.react.com/api/user.php?id=1&type=2', {
-        fetch(data.url, {
-            method: 'get'
-            // mode: 'no-cors'
+        fetch('http://hl.react.com/api/user.php', {
+            method: 'POST',
+            // body: JSON.stringify(data.data),
+            body: formData,
+            mode: 'cors', // 跨域.
+            // headers: {
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Accept': 'application/json',
+            // 'Content-Type': 'application/json',
+            // 'Access-Control-Allow-Origin': '*',
+            // 'Content-Type': 'text/plain'
+            // },
+            credentials: 'same-origin'
         }).then(function(response) {
+            // console.log(response.text());
+            // console.log(response.json());
             return response.json();
         }).then(function(json) {
+            console.log(json);
             if (json.code === 0) { // 登录成功.
                 dispatch({
                     type: LOGIN,
@@ -97,6 +74,33 @@ export const loginFetch = (dispatch, data) => {
             console.log('parsing failed', ex);
             // dispatch();
         });
+
+        // [fetch get]
+        // get params: php $_GET.
+        // fetch('http://localhost:8081/login?id=1&type=2', {
+        // fetch('http://hl.react.com/api/user.php?id=1&type=2', {
+        // fetch(data.url, {
+        //     method: 'get'
+        //     // mode: 'no-cors'
+        // }).then(function(response) {
+        //     console.log(response);
+        //     return response.json();
+        // }).then(function(json) {
+        //     if (json.code === 0) { // 登录成功.
+        //         dispatch({
+        //             type: LOGIN,
+        //             data: {
+        //                 token: json.token,
+        //                 userinfo: json.userinfo
+        //             }
+        //         });
+        //     } else { // 登录失败.
+        //         // dispatch();
+        //     }
+        // }).catch(function(ex) {
+        //     console.log('parsing failed', ex);
+        //     // dispatch();
+        // });
 
         // [axios].
         // axiosPost(data.url, {
